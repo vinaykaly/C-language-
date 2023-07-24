06cc5773ac0f70c507480cc0f2b4ef3cc4fc074e
A VINAY KALYAN REDDY <114601857+vinaykaly@users.noreply.github.com>
2023-07-24 11:25:39 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,25 @@
+/*for a five digit number write a program to obtain the reversed number and to determine whether the original and reversed number are equal or not*/
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void main(){
+
+  int number,x;
+  x = number = 12345;
+  int result = 0;
+
+  while (true){
+
+    if (number !=0){
+
+      result = result*10+number%10;
+      number = number/10;
+                    }
+    else{
+        ( result == x)? printf("it is a palindrome of digit"): printf("not a palindrome of digit);
+      
+         }
+    }
+} 
